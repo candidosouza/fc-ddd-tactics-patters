a85d03004ea5a8bff84b1eb3afd534ba56ac3a0a
candidosouza <candidosouzza@gmail.com>
2022-08-17 08:50:55 -0300

    test: fix of the test description

--- src/domain/checkout/order.spec.ts
+++ src/domain/checkout/order.spec.ts
@@ -14,7 +14,7 @@ describe("Order unit tests", () => {
     }).toThrowError("CustomerId is required");
   });
 
-  it("should throw error when customerId is empty", () => {
+  it("should throw error when item is empty", () => {
     expect(() => {
       let order = new Order("123", "123", []);
     }).toThrowError("Items are required");
@@ -24,9 +24,7 @@ describe("Order unit tests", () => {
     const item = new OrderItem("i1", "Item 1", 100, "p1", 2);
     const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
     const order = new Order("o1", "c1", [item]);
-
     let total = order.total();
-
     expect(order.total()).toBe(200);
 
     const order2 = new Order("o1", "c1", [item, item2]);
