72f3e66ee6b498f35cdd27b0190be3e18ad93ccc
candidosouza <candidosouzza@gmail.com>
2022-08-25 08:53:29 -0300

    test: tests product service

---
+++ src/domain/product/service/product.service.spec.ts
@@ -0,0 +1,13 @@
+import Product from "../entity/product";
+import ProductService from "../service/product.service";
+
+describe("Order service tets", () => {
+  it("should change the prices of all products", () => {
+    const product1 = new Product("xpto1", "Product 1", 10)
+    const product2 = new Product("xpto2", "Product 2", 20)
+    const products = [product1, product2];
+    ProductService.increasePrice(products, 100)
+    expect(product1.price).toBe(20)
+    expect(product2.price).toBe(40)
+  });
+})
