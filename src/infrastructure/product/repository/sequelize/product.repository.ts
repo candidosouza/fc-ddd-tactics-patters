996fb799f60a6595c51d1f8f0d8d9357241b3f35
candidosouza <candidosouzza@gmail.com>
2022-08-30 08:00:17 -0300

    feat: find product repository

--- src/infrastructure/product/repository/sequelize/product.repository.ts
+++ src/infrastructure/product/repository/sequelize/product.repository.ts
@@ -21,7 +21,8 @@ export default class ProductRepository implements ProductRepositoryInterface {
     })
   }
   async find(id: string): Promise<Product> {
-    throw new Error("Method not implemented.")
+    const productModel = await ProductModel.findOne({ where: { id: id } })
+    return new Product(productModel.id, productModel.name, productModel.price)
   }
   async findAll(): Promise<Product[]> {
     throw new Error("Method not implemented.")
